7f72d49e7c423e297ba2bf88bfd15657a87fc5de
Evander <43015355+EvanderLatine@users.noreply.github.com>
2018-12-19 14:47:13 +0300

    Source code of second task ControlWork.Winter.2018-master

--- Source.cpp
+++ Source.cpp
@@ -2,99 +2,108 @@
 
 using namespace std;
 
-const int N = 100;
-
-int nextSmallerThan(int);
-void nextSmallerThanTests();
-void swap(int*, int);
-void swap(int&, int&);
+void encoding(char[], int[], char[]);
+void decoding(int[], char[], char[]);
+void codingTests();
+bool equals(char[], char[]);
+int getLength(char*);
 int getLength(int*);
 
 int main()
 {
-	nextSmallerThanTests();
+	codingTests();
 }
 
-int nextSmallerThan(int number)
+void encoding(char source[], int encoding[], char rule[])
 {
-	int digits[N] = { '\0' };
-	int numberCopy = number,
-		i = 0;
+	int length = getLength(source);
 
-	while (numberCopy > 0)
+	for (int i = 0; i < length; i++)
 	{
-		digits[i] = numberCopy % 10;
-		numberCopy /= 10;
-		i++;
-	}
+		int j = 0;
 
-	int length = i;
-	swap(digits, length);
-
-	for (int i = length - 1; i > 0; i--)
-	{
-		int j = 1;
-
-		while (digits[i] > digits[i - j] && i - j >= 0 && digits[1] != 0)
+		while (source[i] != rule[j])
 		{
 			j++;
 		}
 
-		if (i - j >= 0)
+		encoding[i] = j;
+	}
+}
+
+void decoding(int source[], char decoding[], char rule[])
+{
+	int length = getLength(source);
+
+	for (int i = 0; i < length; i++)
+	{
+		decoding[i] = rule[source[i]];
+	}
+}
+
+bool equals(char lhs[], char rhs[])
+{
+	int i = 0;
+
+	while (lhs[i])
+	{
+		if (lhs[i] != rhs[i])
 		{
-			swap(digits[i], digits[i - j]);
-			break;
+			return false;
 		}
+
+		i++;
 	}
 
-	int result = 0,
-		multiplier = 1;
-
-	for (int i = length - 1; i >= 0; i--)
-	{
-		result += digits[i] * multiplier;
-		multiplier *= 10;
-	}
-
-	if (result < number)
-	{
-		return result;
-	}
-
-	return -1;
+	return true;
 }
 
-void nextSmallerThanTests()
+void codingTests()
 {
-	cout << (nextSmallerThan(21) == 12) << endl;
-	cout << (nextSmallerThan(531) == 513) << endl;
-	cout << (nextSmallerThan(2071) == 2017) << endl;
-	cout << (nextSmallerThan(9) == -1) << endl;
-	cout << (nextSmallerThan(111) == -1) << endl;
-	cout << (nextSmallerThan(135) == -1) << endl;
-	cout << (nextSmallerThan(1027) == -1) << endl;
-	cout << (nextSmallerThan(20026731) == 20026713) << endl;
-	cout << (nextSmallerThan(5303601) == 5303106) << endl;
-	cout << (nextSmallerThan(272371) == 272317) << endl;
-	cout << (nextSmallerThan(10000) == -1) << endl;
-	cout << (nextSmallerThan(22555555) == -1) << endl;
-	cout << (nextSmallerThan(37593) == 37539) << endl;
-	cout << (nextSmallerThan(12345678) == -1) << endl;
+	const int length = 11;
+	const int length1 = 28;
+	const int ruleLength = 30;
+
+	char source[length] = "learn c++!";
+
+	int numbers[length] = { 0 };
+
+	char rule[ruleLength] = "zyxwvutsrqponmlkjihgfedcba!+ ";
+
+	encoding(source, numbers, rule);
+
+	char expected[length];
+
+	decoding(numbers, expected, rule);
+
+	cout << equals(source, expected) << endl;
+
+	// My custom test
+
+	char source1[length1] = "I beleve it would be right!";
+
+	int numbers1[length1] = { 0 };
+	
+	encoding(source1, numbers1, rule);
+
+	char expected1[length1];
+
+	decoding(numbers1, expected1, rule);
+
+	cout << equals(source1, expected1) << endl;
+
 }
 
-void swap(int& left, int& right)
+int getLength(char* source)
 {
-	left += right;
-	right = left - right;
-	left -= right;
-}
+	int length = 0;
 
-void swap(int* arr, int length)
-{
-	for (int i = 0; i < length / 2; i++)
+	while (source[length]) // \ - escape sequence | source[length] != '\0' == source[length]
 	{
-		swap(arr[i], arr[length - 1 - i]);
+		length++;
 	}
+
+	return length;
 }
 
 int getLength(int* source)
